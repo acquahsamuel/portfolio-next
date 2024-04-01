6fb5b7763d8808185b365782f45bfa45bec8554d
acquahsamuel <iamsamuelacquah@gmail.com>
2024-04-01 14:40:00 +0000

    WIP] +> portfolio with blog

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div>
+      <ul className="header">
+        <li> Home </li>
+        <li> Work </li>
+        <li> blog </li>
+        <li> Home </li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,4 @@
+export default function SignInpage() {
+    return <div>  Login  </div>;
+  }
+  
