021487f1e376b45f0927c73c25f98b50fcbbfd53
acquahsamuel <iamsamuelacquah@gmail.com>
2024-04-19 09:46:14 +0000

    WIP] +> frontend protofolio upgrade

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import CardSideHustle from "../../../components/cardSideHustle";
 
-export const OPENSOURCE_PROJECTS = [
+ const OPENSOURCE_PROJECTS = [
   {
     logo_url:
       "https://images.unsplash.com/photo-1542831371-d531d36971e6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
